fe6f330c620602010f9a73e96df716e0af1afacc
LIU-YUXI <1162908914@qq.com>
2023-07-04 15:45:50 +0800

    add PDSN test

--- data_processor/test_dataset.py
+++ data_processor/test_dataset.py
@@ -52,3 +52,40 @@ class CommonTestDataset(Dataset):
         image = (image.transpose((2, 0, 1)) - self.mean) / self.std
         image = torch.from_numpy(image.astype(np.float32))
         return image, image_clip, short_image_path
+
+class CommonTestDataset96(Dataset):
+    """ Data processor for model evaluation.
+
+    Attributes:
+        image_root(str): root directory of test set.
+        image_list_file(str): path of the image list file.
+        crop_eye(bool): crop eye(upper face) as input or not.
+    """
+    def __init__(self, image_root, image_list_file, crop_eye=False, preprocess=None):
+        self.image_root = image_root
+        self.image_list = []
+        image_list_buf = open(image_list_file)
+        line = image_list_buf.readline().strip()
+        while line:
+            self.image_list.append(line)
+            line = image_list_buf.readline().strip()
+        self.mean = 127.5
+        self.std = 128.0
+        self.crop_eye = crop_eye
+        self.preprocess = preprocess
+    def __len__(self):
+        return len(self.image_list)
+    def __getitem__(self, index):
+        short_image_path = self.image_list[index]
+        image_path = os.path.join(self.image_root, short_image_path)
+        image = cv2.imdecode(np.fromfile(image_path, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        if(self.preprocess!=None):
+            image_clip=self.preprocess(Image.fromarray(np.uint8(image)))
+        else:
+            image_clip=image
+        image = cv2.resize(image, (112, 96))
+        if self.crop_eye:
+            image = image[:60, :]
+        image = (image.transpose((2, 0, 1)) - self.mean) / self.std
+        image = torch.from_numpy(image.astype(np.float32))
+        return image, image_clip, short_image_path
---
+++ test_protocol/test_lfw_PDSN.py
@@ -0,0 +1,120 @@
+""" 
+@author: Jun Wang
+@date: 20201012
+@contact: jun21wangustc@gmail.com
+"""
+
+import os
+import sys
+import argparse
+import yaml
+from prettytable import PrettyTable
+from torch.utils.data import DataLoader
+from lfw.pairs_parser import PairsParserFactory
+from lfw.lfw_evaluator import LFWEvaluator
+from utils.model_loader import ModelLoader
+from utils.extractor.feature_extractor import CommonExtractor
+sys.path.append('..')
+from data_processor.test_dataset import CommonTestDataset96
+from backbone.backbone_def import BackboneFactory
+from backbone.iresnet import iresnet100
+import torch
+import clip
+import random
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import backbone.net_sia as net_sia
+# from backbone.SRT.model import SingleLayerModel
+# sys.path.append('/CIS20/lyx/Self-restrained-Triplet-Loss-master')
+# from facenet_pytorch import InceptionResnetV1
+def accu_key(elem):
+    return elem[1]
+
+class FaceModel(torch.nn.Module):
+    """Define a traditional face model which contains a backbone and a head.
+    
+    Attributes:
+        backbone(object): the backbone of face model.
+        head(object): the adapt layer of face model.
+        clip_model(object)
+    """
+    def __init__(self, backbone):
+        super(FaceModel, self).__init__()
+        # self.pre_model = pre_model
+        self.backbone = backbone
+
+    def forward(self, data, data_clip):
+        # pred = F.normalize(self.pre_model(data))
+        f_masked, focc_masked, output, output_occ, f_diff, out = self.backbone.forward(data,data)
+        return output
+
+if __name__ == '__main__':
+    conf = argparse.ArgumentParser(description='lfw test protocal.')
+    conf.add_argument("--test_set", type = str, 
+                      help = "lfw, cplfw, calfw, agedb, rfw_African, \
+                      rfw_Asian, rfw_Caucasian, rfw_Indian.")
+    conf.add_argument("--data_conf_file", type = str, 
+                      help = "the path of data_conf.yaml.")
+    conf.add_argument("--backbone_type", type = str, 
+                      help = "Resnet, Mobilefacenets..")
+    conf.add_argument("--backbone_conf_file", type = str, 
+                      help = "The path of backbone_conf.yaml.")
+    conf.add_argument('--batch_size', type = int, default = 1024)
+    conf.add_argument('--model_path', type = str, default = 'mv_epoch_8.pt', 
+                      help = 'The path of model or the directory which some models in.')
+    conf.add_argument('--pre_model_path', type = str, default = 'mv_epoch_8.pt', 
+                      help = 'The path of model or the directory which some models in.')
+    conf.add_argument('--adapt_path', type = str, default = 'mv_epoch_8.pt', 
+                      help = 'The path of adapt layer or the directory which some models in.')
+    conf.add_argument('--embedding_size', type = int, default = 512, 
+                      help='embedding size of backbones.')
+    conf.add_argument("--device", type = str, default='cuda:0',
+                      help = "dviece.")
+    args = conf.parse_args()
+    # parse config.
+    with open(args.data_conf_file) as f:
+        data_conf = yaml.load(f, Loader=yaml.FullLoader)[args.test_set]
+        pairs_file_path = data_conf['pairs_file_path']
+        cropped_face_folder = data_conf['cropped_face_folder']
+        image_list_file_path = data_conf['image_list_file_path']
+
+    clip_model, preprocess = clip.load("RN50x16", device=torch.device(args.device))
+    clip_model.eval()
+
+    # define pairs_parser_factory
+    pairs_parser_factory = PairsParserFactory(pairs_file_path, args.test_set)
+    # define dataloader
+    data_loader = DataLoader(CommonTestDataset96(cropped_face_folder, image_list_file_path, False, preprocess), 
+                             batch_size=args.batch_size, num_workers=4, shuffle=False)
+    #model def
+    backbone_factory = BackboneFactory(args.backbone_type, args.backbone_conf_file)
+    model_loader = net_sia.LResNet50E_IR_Sia(is_gray=False)
+    # pre_model_loader = iresnet100(num_features=args.embedding_size)# ModelLoader(backbone_factory)
+    feature_extractor = CommonExtractor(args.device)
+    lfw_evaluator = LFWEvaluator(data_loader, pairs_parser_factory, feature_extractor)
+    if os.path.isdir(args.model_path):
+        accu_list = []
+        model_name_list = os.listdir(args.model_path)
+        for model_name in model_name_list:
+            if model_name.endswith('.pt'):
+                model_path = os.path.join(args.model_path, model_name)
+                model_loader.load_state_dict(torch.load(model_path))
+                model = model_loader.cuda(args.device)
+                mean, std = lfw_evaluator.test(model)
+                accu_list.append((os.path.basename(model_path), mean, std))
+        accu_list.sort(key = accu_key, reverse=True)
+    else:
+        # pre_model_loader.load_state_dict(torch.load(args.pre_model_path,map_location='cpu'))
+        # pre_model_loader=pre_model_loader.load_model(args.pre_model_path)
+        # pre_model = pre_model_loader.cuda(args.device)
+        # pre_model = InceptionResnetV1(pretrained='casia-webface').cuda(args.device).eval()
+        model_loader.load_state_dict(torch.load(args.model_path,map_location='cpu')['model_state_dict'])# ['state_dict']
+        model = model_loader.cuda(args.device)
+        test_model = FaceModel(model)
+        mean, std = lfw_evaluator.test(test_model)
+        accu_list = [(os.path.basename(args.model_path), mean, std)]
+    pretty_tabel = PrettyTable(["model_name", "mean accuracy", "standard error"])
+    for accu_item in accu_list:
+        pretty_tabel.add_row(accu_item)
+    print(pretty_tabel)
